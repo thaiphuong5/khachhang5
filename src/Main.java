e8d67344b4500fd5f4b266ffa48d03e37e6a4f64
TranThaiPhuong <phuong.kenfive@gmail.com>
2012-10-22 01:33:57 +0700

    first love

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+
+
+	public static void main(String[] args) {
+		Vector<KhachHang> v=new Vector<KhachHang>();
+		Iterator<KhachHang> it=v.iterator();
+
+	}
+
+}
